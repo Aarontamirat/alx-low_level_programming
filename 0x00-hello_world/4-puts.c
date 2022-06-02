717ab46deed6b499640d94c169f4c1aa45631b66
Aaron <aarontamirat99@gmail.com>
2022-06-02 08:36:47 -0700

    The first program of C in alx that puts a sentence.

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+ * Outputs the sentence (Programming is like building a multilingual puzzle) to the standard output. 
+ * Returns zero if program exits properly.
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return 0;
+}
