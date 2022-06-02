f697462bb39328322d6959e536e55dcd049d3b7d
Aaron <aarontamirat99@gmail.com>
2022-06-02 09:05:13 -0700

    using printf to display a phrase

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - prints the sentence with proper grammer, but the outcome is a piece of art
+ * Return: 0 if exists properly, non zero otherwise
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
