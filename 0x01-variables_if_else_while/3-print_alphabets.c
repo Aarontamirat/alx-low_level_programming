8ed978628c9bd069336e26afb307a8de2d78ad56
Aaron <aarontamirat99@gmail.com>
2022-06-03 02:44:02 -0700

    Displays lower and upper alpabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,14 +6,12 @@
  */
 int main(void)
 {
-        int alphabets;
+	int alphabets;
 
 	for (alphabets = 'a'; alphabets <= 'z'; alphabets++)
 		putchar(alphabets);
 	for (alphabets = 'A'; alphabets <= 'Z'; alphabets++)
 		putchar(alphabets);
-
 	putchar('\n');
-        return (0);
+	return (0);
 }
-
