cc80a5784d53c2d696497b26babb028e6d919451
Aaron <aarontamirat99@gmail.com>
2022-06-03 02:23:11 -0700

    Prints hexadecimals

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - print hexadecimal numbers
+ * Return: 0 upon success
+ */
+int main(void) 
+{
+	int  n;
+	char alphabet;
+	for (n = 46; n < 58; n++)
+	{
+		putchar(n);
+	}
+	for (alphabet = 'a'; alphabet<= 'f'; alphabet++)
+	{
+		putchar(alphabet);
+	}
+	putchar('\n');
+	return (0);
+}
