4c21f626379fcf617edb39c76390ab775dbe3bcf
Aaron <aarontamirat99@gmail.com>
2022-06-03 01:48:10 -0700

    prints out the numbers between 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints single digit numbers
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	int n;
+	for (n = 0; n < 10; n++) {
+		printf("%d", n);
+	}
+	printf("\n");
+	return (0);
+}
