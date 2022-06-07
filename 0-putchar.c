b261a0d91de4c378b7ceb431df146855dfce3972
Aaron <aarontamirat99@gmail.com>
2022-06-07 06:20:21 -0700

    Added the _putchar printer

---
+++ 0-putchar.c
@@ -0,0 +1,22 @@
+nclude "main.h"
+
+/**
+ *  * main - printing out _putchar
+ *   *
+ *    * Description: this program simply prints _putchar
+ *     *
+ *      * Return: 0 up on success
+ *       */
+
+int main(void)
+{
+	char text[9] = "_putchar";
+	int i = 0;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(text[i]);								}
+	_putchar('\n');
+	
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Read me of Functions and nested loops
