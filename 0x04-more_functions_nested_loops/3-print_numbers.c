8b83189294e5ab03a51fce53be21990f67107d38
Aaron <aarontamirat99@gmail.com>
2022-06-09 06:23:25 -0700

    commited the third

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers from 0 - 9
+ * Return: 0 up on success.
+ */
+
+int print_numbers(void)
+{
+	int i;
+
+	for(i = 0; i < 10; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
