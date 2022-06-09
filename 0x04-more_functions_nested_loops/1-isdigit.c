4944fb2cd4fea5cb60f8d0483db1bfe350ccdf7c
Aaron <aarontamirat99@gmail.com>
2022-06-09 05:43:03 -0700

    done with this too

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isdigit - checking whether a param is from 0-9
+ * @c: number to be checked
+ * Return: 1 if it is a digit, 0 if not.
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 46 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multipliestwo integers
+ * @a : is the first number
+ * @b : is the second number
+ * Return: 0 up on success
+ */
+
+int mul(int a, int b)
+{
+	int result = a * b;
+	return (result);
+}
