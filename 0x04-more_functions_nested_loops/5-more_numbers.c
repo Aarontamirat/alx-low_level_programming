07a9c11a78dabc9cc09eccf291001f80e67fab8f
Aaron <aarontamirat99@gmail.com>
2022-06-09 07:06:26 -0700

    fixing fifth

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * more_numbers - prints numbers between 0 to 14 10x
- * 
- * Return: void
+ * more_numbers - prints numbers between 0 to 14
+ * 10 times.
+ * Return: no return.
  */
 void more_numbers(void)
 {
