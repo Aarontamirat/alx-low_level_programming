90c13ac35f275ddcce27013dbebd7d36d83ecdce
Aaron <aarontamirat99@gmail.com>
2022-06-13 02:34:25 -0700

    Fixed this one

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,11 +8,17 @@
 
 void print_rev(char *s)
 {
-	int i;
+	int i = 0;
 
-	for(i = sizeof(s); i > 0; i++)
+	while (s[i] != '\0')
 	{
-		putchar(s[i]);
+		i++;
 	}
-	putchar('\n');
+	i--;
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
 }
