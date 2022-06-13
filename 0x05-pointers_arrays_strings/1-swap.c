42ef2d7a08ea38dffb405ceadb04be9a8b6ccdf3
Aaron <aarontamirat99@gmail.com>
2022-06-13 01:09:20 -0700

    Commited swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main"
+
+/**
+ * swap_int - integer value swapping
+ * @a: pointer one
+ * @b: pointer two
+ * Return: void;
+ */
+
+void swap_int(int *a, int *b)
+{
+	int temp;
+	*a = temp;
+	*a = *b;
+	*b = temp;
+}	
