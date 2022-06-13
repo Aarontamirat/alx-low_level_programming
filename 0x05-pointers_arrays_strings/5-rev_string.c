c3c561ad21d9e3fad236952124e04cc6646579fd
Aaron <aarontamirat99@gmail.com>
2022-06-13 03:00:47 -0700

    Reversing string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/* rev_string(char *s) - reverses strings
+ * @s: an input pointer
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int i, maximum, medium;
+	char first, last;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	maximum = i - 1;
+	medium = maximum / 2;
+	while (medium >= 0)
+	{
+		first = s[maximum - medium];
+		last = s[medium];
+		s[medium] = first;
+		s[maximum - medium] = last;
+		medium--;
+	}
+}
