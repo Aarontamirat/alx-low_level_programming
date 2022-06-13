b35773608f83bc31bf89fec0800829440c15afee
Aaron <aarontamirat99@gmail.com>
2022-06-13 03:49:53 -0700

    seventh putting half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts_half - Prints out half the string
+ * @str: input characters
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int size;
+
+	for(size = sizeof(str)/2; size <sizeof(str); size ++)
+	{
+		putchar(str[size]);
+	}
+	putchar('\n');
+}
