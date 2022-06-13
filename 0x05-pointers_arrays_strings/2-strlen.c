978cea4bccdb0bd3a199f3ec87aff9e56a15f1ca
Aaron <aarontamirat99@gmail.com>
2022-06-13 01:19:37 -0700

    String length returning

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strlen - return string length passed by pointer
+ * @s: pointer
+ * Return: length;
+ */
+
+int _strlen(char *s)
+{
+	int len = strlen(s);
+	return (len);
+}
