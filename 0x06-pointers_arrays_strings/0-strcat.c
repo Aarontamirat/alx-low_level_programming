01898d56f97b74cdb7a04129ae05d83eec011ab9
Aaron <aarontamirat99@gmail.com>
2022-06-15 01:06:52 -0700

    String concatination

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings.
+ * @dest: passed string array.
+ * @src: passed string array.
+ * Return: concatenated value.
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
