39877c655405e283c2276cced3a52767ac20c6f8
Aaron <aarontamirat99@gmail.com>
2022-06-15 23:22:06 -0700

    Converts passed in arrays of chars of small letters to capital letters

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * string_toupper - converts small letters to capital
+ * @s: input string
+ * Return: uppercase string s
+ */
+
+char *string_toupper(char *s)
+{
+	int count = 0;
+
+	while (*(s + count) != '\0')
+	{
+		if ((*(s + count) >= 97) && (*(s + count) <= 122))
+			*(s + count) = *(s + count) - 32;
+		count++;
+	}
+
+	return (s);
+}
