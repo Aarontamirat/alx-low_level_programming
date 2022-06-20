85b4aac7bec4501b9edc8d1b4575da879df4be4d
Aaron <aarontamirat99@gmail.com>
2022-06-20 05:23:23 -0700

    String matching

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strstr - finds string from string
+ * @haystack: array source
+ * @needle: word to match
+ * Return: the match or NULL
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	char *result = haystack, *fneedle = needle;
+
+	while (*haystack)
+	{
+		while (*needle)
+		{
+			if (*haystack++ != *needle++)
+			{
+				break;
+			}
+		}
+		if (!*needle)
+		{
+			return (result);
+		}
+		needle = fneedle;
+		result++;
+		haystack = result;
+	}
+	return (0);
+}
