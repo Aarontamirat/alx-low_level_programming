afaa7453b13fe9c6990af9a5aabe3763e936fbc5
Aaron <aarontamirat99@gmail.com>
2022-06-19 23:57:14 -0700

    Copying from buffer

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies from buffer to another
+ * @dest: dstination char array
+ * @src: source char array
+ * @n: bytes from memory area
+ * Return: dest array
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src [i];
+	}
+	return (dest);
+}
