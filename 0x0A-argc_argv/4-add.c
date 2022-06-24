2e5f3d6f36b8c67abcb3804f29f24b56e2273b21
Aaron <aarontamirat99@gmail.com>
2022-06-24 10:17:36 -0700

    Adding positive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - sum of positive numbers.
+ * @argc: number of cmd args.
+ * @argv: a string of cmd line args
+ * Return: 0 upon success.
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j, sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", add);
+	return (0);
+}
