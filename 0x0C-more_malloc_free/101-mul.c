4e726cb25783426df2e69c80f279fe1d393ac1dc
Aaron <aarontamirat99@gmail.com>
2022-06-30 05:38:31 -0700

    Adding two numbers using malloc

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * main - multiplying two numbers using malloc
+ * @argc: number of arguments
+ * @argv: arguments passed in
+ * Return: integers
+ */
+
+int main(int argc, char *argv[])
+{
+unsigned long product;
+int i, j;
+	if (argc != 3)
+	{ printf("Error\n");
+	exit(98); }
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] > 57 || argv[i][j] < 48)
+			{  printf("Error\n");
+			exit(98); }
+		}
+
+	}
+	product = atol(argv[1]) *  atol(argv[2]);
+	printf("%lu\n", product);
+return (0);
+}
