f875b50be6fac7ec24093524068b2e3b925f5ae1
Aaron <aarontamirat99@gmail.com>
2022-06-30 05:45:08 -0700

    Adding two numbers using malloc

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -9,18 +9,23 @@
 
 int main(int argc, char *argv[])
 {
-unsigned long product;
-int i, j;
+	unsigned long product;
+	int i, j;
+
 	if (argc != 3)
-	{ printf("Error\n");
-	exit(98); }
+	{
+	       	printf("Error\n");
+		exit(98);
+	}
 	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
 			if (argv[i][j] > 57 || argv[i][j] < 48)
-			{  printf("Error\n");
-			exit(98); }
+			{
+				printf("Error\n");
+				exit(98);
+			}
 		}
 
 	}
