33fd297025f69e82f9d57bdd2a587f62d3c02991
Aaron <aarontamirat99@gmail.com>
2022-06-30 12:58:08 -0700

    Spent

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -11,7 +11,9 @@
 
 void _is_zero(char *argv[])
 {
-	int i, isn1 = 1, isn2 = 1;
+	int i;
+	int isn1 = 1;
+	int isn2 = 1;
 
 	for (i = 0; argv[1][i]; i++)
 	if (argv[1][i] != '0')
@@ -81,17 +83,37 @@ int _checknum(char *argv[], int n)
  */
 int main(int argc, char *argv[])
 {
-	int ln1, ln2, lnout, add, addl, i, j, k, ca;
+	int ln1;
+	int ln2; 
+	int lnout;
+	int add;
+	int addl;
+       	int i;
+	int j;
+	int k;
+	int ca;
 	char *nout;
 
 	if (argc != 3)
-		printf("Error\n"), exit(98);
-	ln1 = _checknum(argv, 1), ln2 = _checknum(argv, 2);
-	_is_zero(argv), lnout = ln1 + ln2, nout = malloc(lnout + 1);
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	ln1 = _checknum(argv, 1);
+	ln2 = _checknum(argv, 2);
+	_is_zero(argv);
+	lnout = ln1 + ln2;
+	nout = malloc(lnout + 1);
 	if (nout == NULL)
-		printf("Error\n"), exit(98);
+	{
+		printf("Error\n");
+		exit(98);
+	}
 	nout = _initialize_array(nout, lnout);
-	k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
+	k = lnout - 1;
+	i = ln1 - 1;
+	j = ln2 - 1;
+	ca = addl = 0;
 	for (; k >= 0; k--, i--)
 	{
 		if (i < 0)
@@ -103,20 +125,30 @@ int main(int argc, char *argv[])
 					nout[k - 1] = (add / 10) + '0';
 				nout[k] = (add % 10) + '0';
 			}
-			i = ln1 - 1, j--, addl = 0, ca++, k = lnout - (1 + ca);
+			i = ln1 - 1;
+			j--;
+			addl = 0;
+			ca++;
+			k = lnout - (1 + ca);
 		}
 		if (j < 0)
 		{
 			if (nout[0] != '0')
 				break;
 			lnout--;
-			free(nout), nout = malloc(lnout + 1), nout = _initialize_array(nout, lnout);
-			k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
+			free(nout);
+			nout = malloc(lnout + 1);
+			nout = _initialize_array(nout, lnout);
+			k = lnout - 1;
+			i = ln1 - 1;
+			j = ln2 - 1;
+			ca = addl = 0;
 		}
 		if (j >= 0)
 		{
 			add = ((argv[1][i] - '0') * (argv[2][j] - '0')) + (nout[k] - '0') + addl;
-			addl = add / 10, nout[k] = (add % 10) + '0';
+			addl = add / 10;
+			nout[k] = (add % 10) + '0';
 		}
 	}
 	printf("%s\n", nout);
