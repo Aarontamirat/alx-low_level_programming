0e3c5ca3d8c9e109a38bdf940ed649d269c8df66
Aaron <aarontamirat99@gmail.com>
2022-06-30 07:23:16 -0700

    Multiplies two numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,13 +2,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <ctype.h>
-
 /**
  * _is_zero - determines if any number is zero
  * @argv: argument vector.
  *
  * Return: no return.
  */
+
 void _is_zero(char *argv[])
 {
 	int i, isn1 = 1, isn2 = 1;
@@ -41,6 +41,7 @@ void _is_zero(char *argv[])
  *
  * Return: pointer of a char array.
  */
+
 char *_initialize_array(char *ar, int lar)
 {
 	int i = 0;
@@ -59,6 +60,7 @@ char *_initialize_array(char *ar, int lar)
  *
  * Return: length of the number.
  */
+
 int _checknum(char *argv[], int n)
 {
 	int ln;
@@ -87,9 +89,15 @@ int main(int argc, char *argv[])
 	char *nout;
 
 	if (argc != 3)
-		printf("Error\n"), exit(98);
-	ln1 = _checknum(argv, 1), ln2 = _checknum(argv, 2);
-	_is_zero(argv), lnout = ln1 + ln2, nout = malloc(lnout + 1);
+	{
+		printf("Error\n");
+       		exit(98);
+	}
+	ln1 = _checknum(argv, 1); 
+	ln2 = _checknum(argv, 2);
+	_is_zero(argv);
+       	lnout = ln1 + ln2;
+       	nout = malloc(lnout + 1);
 	if (nout == NULL)
 		printf("Error\n"), exit(98);
 	nout = _initialize_array(nout, lnout);
@@ -105,20 +113,30 @@ int main(int argc, char *argv[])
 					nout[k - 1] = (add / 10) + '0';
 				nout[k] = (add % 10) + '0';
 			}
-			i = ln1 - 1, j--, addl = 0, ca++, k = lnout - (1 + ca);
+			i = ln1 - 1;
+		       	j--; 
+			addl = 0; 
+			ca++; 
+			k = lnout - (1 + ca);
 		}
 		if (j < 0)
 		{
 			if (nout[0] != '0')
 				break;
 			lnout--;
-			free(nout), nout = malloc(lnout + 1), nout = _initialize_array(nout, lnout);
-			k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
+			free(nout); 
+			nout = malloc(lnout + 1);
+		       	nout = _initialize_array(nout, lnout);
+			k = lnout - 1;
+		       	i = ln1 - 1;
+			j = ln2 - 1;
+			ca = addl = 0;
 		}
 		if (j >= 0)
 		{
 			add = ((argv[1][i] - '0') * (argv[2][j] - '0')) + (nout[k] - '0') + addl;
-			addl = add / 10, nout[k] = (add % 10) + '0';
+			addl = add / 10;
+			nout[k] = (add % 10) + '0';
 		}
 	}
 	printf("%s\n", nout);
