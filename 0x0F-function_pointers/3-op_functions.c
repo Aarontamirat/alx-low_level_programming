a529927fbcda9765e19f6adaba603e68e49996ff
Aaron <aarontamirat99@gmail.com>
2022-07-06 03:27:03 -0700

    Holds all the operation functions

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - sum calculator
+ * @a: integer
+ * @b: integer
+ * Return: sum
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - difference calculator
+ * @a: integer
+ * @b: integer
+ * Return: subtraction
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - product calculator
+ * @a: integer
+ * @b: integer
+ * Return: product
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - division calculator
+ * @a: integer
+ * @b: integer
+ * Return: division
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - remainder calculator
+ * @a: integer
+ * @b: integer
+ * Return: remainder
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
