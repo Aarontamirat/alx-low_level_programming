c149754f1780563a95666647aab95f841d5fec92
Aaron <aarontamirat99@gmail.com>
2022-07-14 08:30:04 -0700

    Add elements at the start of a linkled list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * add_node - adds a new node at the beginning of 
+ * add_node - adds a new node at the beginning of
  * a list_t list
  * @head: head of the linked list.
  * @str: string to store in the list.
@@ -9,21 +9,21 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new;
-	size_t nchar;
+	list_t *tempo;
+	size_t leng;
 
-	new = malloc(sizeof(list_t));
-	if (new == NULL)
+	tempo = malloc(sizeof(list_t));
+	if (tempo == NULL)
 		return (NULL);
 
-	new->str = strdup(str);
+	tempo->str = strdup(str);
 
-	for (nchar = 0; str[nchar]; nchar++)
+	for (leng = 0; str[leng]; leng++)
 		;
 
-	new->len = nchar;
-	new->next = *head;
-	*head = new;
+	tempo->len = leng;
+	tempo->next = *head;
+	*head = tempo;
 
 	return (*head);
 }
