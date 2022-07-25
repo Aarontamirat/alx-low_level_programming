4a139dc703e172e5d6ac6324d7515a8f55c8d237
Aaron <aarontamirat99@gmail.com>
2022-07-25 03:10:17 -0700

    Copying text from file to file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,24 +2,13 @@
 #include <stdio.h>
 
 /**
- * error_file - accessibility checker
- * @file_from: source.
- * @file_to: destination.
- * @argv: arguments vector.
- * Return: void.
- */
-
-#include "main.h"
-#include <stdio.h>
-
-/**
  * error_file - checks if files can be opened.
  * @file_from: file_from.
  * @file_to: file_to.
  * @argv: arguments vector.
  * Return: no return.
  */
-void file_check(int file_from, int file_to, char *argv[])
+void error_file(int file_from, int file_to, char *argv[])
 {
 	if (file_from == -1)
 	{
@@ -34,16 +23,15 @@ void file_check(int file_from, int file_to, char *argv[])
 }
 
 /**
- * main - copies the content of a file to another file
+ * main - check the code for Holberton School students.
  * @argc: number of arguments.
  * @argv: arguments vector.
  * Return: Always 0.
  */
-
 int main(int argc, char *argv[])
 {
 	int file_from, file_to, err_close;
-	ssize_t chars, wr;
+	ssize_t nchars, nwr;
 	char buf[1024];
 
 	if (argc != 3)
@@ -54,17 +42,17 @@ int main(int argc, char *argv[])
 
 	file_from = open(argv[1], O_RDONLY);
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-	file_check(file_from, file_to, argv);
+	error_file(file_from, file_to, argv);
 
-	chars = 1024;
-	while (chars == 1024)
+	nchars = 1024;
+	while (nchars == 1024)
 	{
-		chars = read(file_from, buf, 1024);
-		if (chars == -1)
-			file_check(-1, 0, argv);
-		wr = write(file_to, buf, chars);
-		if (wr == -1)
-			file_check(0, -1, argv);
+		nchars = read(file_from, buf, 1024);
+		if (nchars == -1)
+			error_file(-1, 0, argv);
+		nwr = write(file_to, buf, nchars);
+		if (nwr == -1)
+			error_file(0, -1, argv);
 	}
 
 	err_close = close(file_from);
