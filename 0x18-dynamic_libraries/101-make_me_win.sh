a033bec7f3c39839f56bd89b1dbd12bb127028df
Aaron <aarontamirat99@gmail.com>
2022-09-06 06:09:23 -0700

    Fixed

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/Aarontamirat/alx-low_level_programming/tree/master/0x18-dynamic_libraries/librarytwo.so
-export LD_PRELOAD=/tmp/librarytwo.so
+wget -P /tmp https://github.com/Aarontamirat/alx-low_level_programming/tree/master/0x18-dynamic_libraries/libmask.so
+export LD_PRELOAD=/tmp/libmask.so
